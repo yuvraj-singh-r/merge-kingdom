b1a7d58a3c7ab37348f03bc5a6c677b1e4eeb3ac
Yuvraj Rajput <yuvrajlodha299@gmail.com>
2026-08-16 02:46:41 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -13,8 +13,14 @@ const CHAINS = {
     {name:"Forge",icon:"🔥"},{name:"Castle Wall",icon:"🧱"}
   ]},
   wood:{label:"Wood", tiers:[
-    {name:"Wood",icon:"🪵"},{name:"Lumber",icon:"🪚"},{name:"House",icon:"🏠"},
-    {name:"Village",icon:"🏘️"},{name:"Town",icon:"🏙️"},{name:"Kingdom",icon:"🏰"}
+  {name:"Stick",icon:"🪵",image:"assets/wood/wood_01_stick.png"},
+  {name:"Wood Log",icon:"🪵",image:"assets/wood/wood_02_log.png"},
+  {name:"Timber Stack",icon:"🪵",image:"assets/wood/wood_03_timber_stack.png"},
+  {name:"Wood Pile",icon:"🪵",image:"assets/wood/wood_04_wood_pile.png"},
+  {name:"Lumber Bundle",icon:"🪵",image:"assets/wood/wood_05_lumber_bundle.png"},
+  {name:"Magic Wood",icon:"✨",image:"assets/wood/wood_06_magic_wood.png"},
+  {name:"Enchanted Timber",icon:"✨",image:"assets/wood/wood_07_enchanted_timber.png"},
+  {name:"Ancient Sacred Wood",icon:"🌳",image:"assets/wood/wood_08_ancient_sacred_wood.png"}
   ]},
   flower:{label:"Flower", tiers:[
     {name:"Flower",icon:"🌸"},{name:"Garden",icon:"💐"},{name:"Farm",icon:"🌻"},{name:"Windmill",icon:"🎡"}
